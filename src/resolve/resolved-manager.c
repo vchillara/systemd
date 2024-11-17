3572b6671117310fdc88b1c9293f48819565f706
Vishal Chillara Srinivas <vishal.chillarasrinivas@philips.com>
2024-11-17 12:43:46 +0530

    resolve: preparatory commit to DnssdService to DnssdRegisteredService

--- src/resolve/resolved-manager.c
+++ src/resolve/resolved-manager.c
@@ -768,7 +768,7 @@ int manager_start(Manager *m) {
 
 Manager *manager_free(Manager *m) {
         Link *l;
-        DnssdService *s;
+        DnssdRegisteredService *s;
         DnsServiceBrowser *sb;
 
         if (!m)
@@ -1350,7 +1350,7 @@ int manager_find_ifindex(Manager *m, int family, const union in_addr_union *in_a
 
 void manager_refresh_rrs(Manager *m) {
         Link *l;
-        DnssdService *s;
+        DnssdRegisteredService *s;
 
         assert(m);
 
@@ -1776,7 +1776,7 @@ void manager_cleanup_saved_user(Manager *m) {
 }
 
 bool manager_next_dnssd_names(Manager *m) {
-        DnssdService *s;
+        DnssdRegisteredService *s;
         bool tried = false;
         int r;
 
